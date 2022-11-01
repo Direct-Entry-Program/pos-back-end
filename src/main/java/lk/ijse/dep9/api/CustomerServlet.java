52bea43994f3eec957324d69860fd673d99da57b
danulatb <balsooriyadt@gmail.com>
2022-11-01 19:30:54 +0530

    save method done

--- src/main/java/lk/ijse/dep9/api/CustomerServlet.java
+++ src/main/java/lk/ijse/dep9/api/CustomerServlet.java
@@ -39,28 +39,46 @@ public class CustomerServlet extends HttpServlet2 {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getPathInfo()==null || req.getPathInfo().equals("/")){
-            if (!req.getContentType().startsWith("application/json")){
-                throw new JsonbException("Invalid Json");
+
+            try {
+
+
+                if (!req.getContentType().startsWith("application/json")) {
+                    throw new JsonbException("Invalid Json");
+                }
+                CustomerDTO customer = JsonbBuilder.create().fromJson(req.getReader(), CustomerDTO.class);
+
+                if (customer.getName() == null || !customer.getName().matches("[A-Za-z ]+")) {
+                    throw new JsonbException("Name is invalid or empty");
+                } else if (customer.getAddress() == null || !customer.getAddress().matches("[A-Za-z0-9;,./\\-]+")) {
+                    throw new JsonbException("Address is invalid or empty");
+                }
+
+                try (Connection connection = pool.getConnection()) {
+                    PreparedStatement stm = connection.prepareStatement("INSERT INTO Customer (id, name, address) VALUES (?,?,?)");
+
+                    stm.setString(1, UUID.randomUUID().toString());
+                    stm.setString(2, customer.getName());
+                    stm.setString(3, customer.getAddress());
+
+                    int updated = stm.executeUpdate();
+
+                    if (updated == 1) {
+                        resp.setStatus(HttpServletResponse.SC_CREATED);
+                        resp.setContentType("application/json");
+                        JsonbBuilder.create().toJson(customer, resp.getWriter());
+                    } else {
+                        throw new SQLException("Not updated, Something went wrong");
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Can not load db , try again");
+                }
+            }catch (JsonbException e){
+                e.printStackTrace();
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST,"Invalid input");
             }
-            CustomerDTO customer = JsonbBuilder.create().fromJson(req.getReader(), CustomerDTO.class);
-
-            if (customer.getName() == null || !customer.getName().matches("[A-Za-z ]+")){
-                throw new JsonbException("Name is invalid or empty");
-            } else if (customer.getAddress() ==null || !customer.getAddress().matches("[A-Za-z0-9;,./\\-]+")) {
-                throw new JsonbException("Address is invalid or empty");
-            }
-
-            try(Connection connection = pool.getConnection()){
-                PreparedStatement stm = connection.prepareStatement("INSERT INTO Customer (id, name, address) VALUES (?,?,?)");
-
-                stm.setString(1,UUID.randomUUID().toString());
-                stm.setString(2,customer.getName());
-                stm.setString(3,customer.getAddress());
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-
 
         }else {
             resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED,"Not Implemented yet");
