fcec1e82e078999f730d1eb672fd725ff52a2f37
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-01 18:50:48 +0530

    Complete get order details logic

--- src/main/java/lk/ijse/dep9/api/OrderServlet.java
+++ src/main/java/lk/ijse/dep9/api/OrderServlet.java
@@ -1,20 +1,29 @@
 package lk.ijse.dep9.api;
 
 import jakarta.annotation.Resource;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.api.util.HttpServlet2;
+import lk.ijse.dep9.dto.OrderCompleteDTO;
+import lk.ijse.dep9.dto.OrderDTO;
+import lk.ijse.dep9.dto.OrderDetailsDTO;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @WebServlet(name = "order-servlet", value = "/orders/*")
 public class OrderServlet extends HttpServlet2 {
-//    @Resource(lookup = "java:/comp/env/jdbc/pos")
-//    private DataSource pool;
+    @Resource(lookup = "java:/comp/env/jdbc/pos")
+    private DataSource pool;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,8 +46,8 @@ public class OrderServlet extends HttpServlet2 {
             Matcher matcher = Pattern.compile("^/([A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12})/?$")
                     .matcher(request.getPathInfo());
             if (matcher.matches()) {
-//                    getOrderDetails(matcher.group(1), response);
-                response.getWriter().println("<h1>Get order details</h1>");
+                    getOrderDetails(matcher.group(1), response);
+//                response.getWriter().println("<h1>Get order details</h1>");
             }
             else {
                 response.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED, "Expected valid UUID");
@@ -50,8 +59,41 @@ public class OrderServlet extends HttpServlet2 {
 
     }
 
-    private void getOrderDetails(String orderId, HttpServletResponse response) {
-
+    private void getOrderDetails(String orderId, HttpServletResponse response) throws IOException {
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement orderStm = connection.prepareStatement("SELECT * FROM Order WHERE id=?");
+            orderStm.setString(1, orderId);
+            ResultSet orderRst = orderStm.executeQuery();
+            OrderDTO orderDTO = null;
+            if (orderRst.next()) {
+                String id = orderRst.getString("id");
+                String date = orderRst.getString("date");
+                String customerId = orderRst.getString("customer_id");
+                orderDTO = new OrderDTO(id, date, customerId);
+            }
+            else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid order id");
+                return;
+            }
+            PreparedStatement orderIdStm = connection.prepareStatement("SELECT  * FROM Order_details WHERE order_id=?");
+            orderIdStm.setString(1, orderId);
+            ResultSet orderIdRst = orderIdStm.executeQuery();
+            OrderDetailsDTO orderDetailsDTO = null;
+            if (orderIdRst.next()) {
+                String orderId1 = orderIdRst.getString("order_id");
+                String itemCode = orderIdRst.getString("item_code");
+                String unitPrice = orderIdRst.getString("unit_price");
+                String qty = orderIdRst.getString("qty");
+                orderDetailsDTO = new OrderDetailsDTO(orderId1, itemCode, unitPrice, qty);
+            }
+            OrderCompleteDTO dto = new OrderCompleteDTO(orderDTO.getId(), orderDTO.getDate(), orderDTO.getCustomerId(), orderDetailsDTO.getItemCode(), orderDetailsDTO.getUnitPrice(), orderDetailsDTO.getQty());
+            response.setContentType("application/json");
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(dto, response.getWriter());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to fetch order details");
+        }
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
