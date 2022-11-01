d8658fa1bb653baacaaa7b2cd92d9f73d4926e58
hiranwj <hiranwj.connect@gmail.com>
2022-11-01 18:22:49 +0530

    Modify loadAllItems

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -22,35 +22,35 @@ public class ItemServlet extends HttpServlet2 {
     }
 
     private void loadAllItems(HttpServletResponse response) throws IOException {
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep9_lms", "root", "mysql")) {
-//                Statement stm = connection.createStatement();
-//                ResultSet rst = stm.executeQuery("SELECT * FROM item");
-//
-//                ArrayList<ItemDTO> members = new ArrayList<>();
-//
-//                while (rst.next()){
-//                    String code = rst.getString("code");
-//                    String stock = rst.getString("stock");
-//                    String unit_price = rst.getString("unit_price");
-//                    String description = rst.getString("description");
-//                    ItemDTO dto = new ItemDTO(code, stock, unit_price, description);
-//                    members.add(dto);
-//                }
-//
-//                Jsonb jsonb = JsonbBuilder.create();
-//                response.setContentType("application/json");
-//                jsonb.toJson(members, response.getWriter());
-//
-//
-//            }
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to fetch items");
-//        }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep9_lms", "root", "mysql")) {
+                Statement stm = connection.createStatement();
+                ResultSet rst = stm.executeQuery("SELECT * FROM item");
 
-        response.getWriter().printf("WS: Load All items");
+                ArrayList<ItemDTO> members = new ArrayList<>();
+
+                while (rst.next()){
+                    String code = rst.getString("code");
+                    String stock = rst.getString("stock");
+                    String unit_price = rst.getString("unit_price");
+                    String description = rst.getString("description");
+                    ItemDTO dto = new ItemDTO(code, stock, unit_price, description);
+                    members.add(dto);
+                }
+
+                Jsonb jsonb = JsonbBuilder.create();
+                response.setContentType("application/json");
+                jsonb.toJson(members, response.getWriter());
+
+
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to fetch items");
+        }
+
+//        response.getWriter().printf("WS: Load All items");
     }
 
     @Override
